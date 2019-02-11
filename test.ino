ec04112868b0c8d4238cdf834d92331cc7ac6c28
OMAR FARUK <umorfarukcse@gmail.com>
2019-02-11 18:24:42 +0600

    documentation change

--- test.ino
+++ test.ino
@@ -1,14 +1,4 @@
 
-/* Omar Faruk, umorfarukcse@gmail.com, 
- *  My Arduino web app link here  https://arduino-f7537.firebaseapp.com
- *  don't be afraid to see 200 lines of code it's very easy, same code you have to write for control all the devices.
- *  1. firstly we to have to install some library for elsp8266 and firebase
- *  2. go to this link and download it,    https://github.com/FirebaseExtended/firebase-arduino
- *  3. than go to your Arduino Ide and click the Sketch function you find it beside Tools, then select your zip file it will take some while for installing 
- *  4.  go to this link and download it, https://github.com/esp8266/Arduino than follow the step 3 instruction 
- *  5. after this you need to go to Tools and click Manage Library than Search for ArduinoJson and install 5.13.2 don't install the latest version
- *  it may create some error while you run your code 
-*/
  
 #include <ESP8266WiFi.h>  //wifi module header
 #include <FirebaseArduino.h>  //firebase header you have to install those two librery
